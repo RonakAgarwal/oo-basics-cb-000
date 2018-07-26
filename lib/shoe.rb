5b984bbbd1ad6d24ea430d5622cac4f1c4de2bc4
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-26 21:18:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,1 @@
-# Make your shoe class here!
-class Book
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
-
-end
